82bb25bbce010bcbecb4ce599d2cfbd3e18ce7c8
brennamaloy <bkm91466@creighton.edu>
2020-10-05 15:15:43 -0500

    Initial commit

---
+++ forms/Animals/Animals.js
@@ -0,0 +1,29 @@
+/* Created a new form named animals.
+Created an array named myAnimals with these animals in it:
+   dog, cat, horse, meerkat
+Got the name of an animal from the user and adds it to the end of the array using an array method. 
+Included code so the user can use any mix of case to enter the animal name. 
+eg. the user could enter:
+        eagle
+        Eagle
+        EAGLE
+        EaglE
+            etc.
+Output the last animal in the array using an array method to an alert using a template literal to the console with the format below. Used the myAnimals array in the template literal to show the last animal added: 
+eg. Say the last animal in the array is 'eagle'. Then the output would be: 
+       "The last animal is a/an eagle."
+Code is all commented out. 
+Program runs without error. */
+/*
+let myAnimals = ["dog", "cat", "horse", "meerkat"]
+
+let enterAnimal = prompt("Enter a name of an animal to add it to the end of the array")
+
+let NewAnimal = enterAnimal.toLowerCase()
+
+myAnimals.push(NewAnimal)
+
+alert(`The last animal is a/an ${myAnimals[myAnimals.length - 1]}.`)
+
+console.log(myAnimals)
+*/
---
+++ forms/IncomeBracket/IncomeBracket.js
@@ -0,0 +1,33 @@
+/* Created a new form named incomeBracket.
+Used the design shown above. 
+Used Bootstrap4 input, label, and button controls. 
+All controls are named appropriately like we did in class (button: btnSomeName).
+NO alerts, prompts, confirms, or console.logs used.
+Got income from user using a control. 
+Used at least one if/else statement.
+Used at least one boolean operator. 
+Used this format for output into a control using a template literal.
+    Example: say the user input:  100000. The output would be:
+             "With your income of $100000, you are in a tax bracket of 25%"
+Code is all commented out. 
+Program runs without error.
+*/
+
+/*
+btnSubmit.onclick=function(){
+  let income = Number(inptIncome.value)
+  if (income < 30000)  
+    lblAnswer.value = `With your income of $${income}  you are in a tax bracket of 8%.`
+   else if (income > 30000 && income < 99999) 
+    lblAnswer.value = `With your income of $${income}  you are in a tax bracket of 15%.`
+   else if (income > 99999) 
+    lblAnswer.value = `With your income of $${income}  you are in a tax bracket of 25%.`
+   else 
+    lblAnswer.value = `You did not enter a value`
+}
+
+btnGoAgain.onclick=function(){
+  inptIncome.value = ""
+  lblAnswer.value = ""
+}
+*/
